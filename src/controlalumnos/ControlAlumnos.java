36a7cb33691f23b246859cceebb497398e6372d5
Gerardo Beltrán Torres <47992313+Conquest19990@users.noreply.github.com>
2021-05-20 16:08:51 -0500

    Add files via upload

---
+++ src/controlalumnos/ControlAlumnos.java
@@ -0,0 +1,13 @@
+package controlalumnos;
+
+/**
+ *
+ * @author alumnos
+ */
+public class ControlAlumnos {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
